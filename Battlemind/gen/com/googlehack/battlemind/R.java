4d3706278ebc04532837bf4aa7ac4353e74f738d
indaso <indaso5@gmail.com>
2014-02-08 01:45:04 -0500

    fixed icon launcher

--- Battlemind/gen/com/googlehack/battlemind/R.java
+++ Battlemind/gen/com/googlehack/battlemind/R.java
@@ -21,15 +21,14 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int battlemindgameicon=0x7f020000;
-        public static final int battlemindlevel1=0x7f020001;
-        public static final int battlemindlevel2=0x7f020002;
-        public static final int battlemindlevel3=0x7f020003;
-        public static final int battlemindlevel4=0x7f020004;
-        public static final int battlemindlevel5=0x7f020005;
-        public static final int battlemindlevel6=0x7f020006;
-        public static final int battlemindlevel7=0x7f020007;
-        public static final int ic_launcher=0x7f020008;
+        public static final int battlemindlevel1=0x7f020000;
+        public static final int battlemindlevel2=0x7f020001;
+        public static final int battlemindlevel3=0x7f020002;
+        public static final int battlemindlevel4=0x7f020003;
+        public static final int battlemindlevel5=0x7f020004;
+        public static final int battlemindlevel6=0x7f020005;
+        public static final int battlemindlevel7=0x7f020006;
+        public static final int ic_launcher=0x7f020007;
     }
     public static final class id {
         public static final int action_settings=0x7f080005;
