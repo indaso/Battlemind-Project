af42f6639c5d674a1e5eab22fe8694f337d9adbb
brechang <brechang@seas.upenn.edu>
2014-02-08 02:13:56 -0500

    added activity view boilerplate

--- Battlemind/gen/com/googlehack/battlemind/R.java
+++ Battlemind/gen/com/googlehack/battlemind/R.java
@@ -9,6 +9,39 @@ package com.googlehack.battlemind;
 
 public final class R {
     public static final class attr {
+        /** <p>Must be a color value, in the form of "<code>#<i>rgb</i></code>", "<code>#<i>argb</i></code>",
+"<code>#<i>rrggbb</i></code>", or "<code>#<i>aarrggbb</i></code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int exampleColor=0x7f010002;
+        /** <p>Must be a dimension value, which is a floating point number appended with a unit such as "<code>14.5sp</code>".
+Available units are: px (pixels), dp (density-independent pixels), sp (scaled pixels based on preferred font size),
+in (inches), mm (millimeters).
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int exampleDimension=0x7f010001;
+        /** <p>May be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+<p>May be a color value, in the form of "<code>#<i>rgb</i></code>", "<code>#<i>argb</i></code>",
+"<code>#<i>rrggbb</i></code>", or "<code>#<i>aarrggbb</i></code>".
+         */
+        public static final int exampleDrawable=0x7f010003;
+        /** <p>Must be a string value, using '\\;' to escape characters such as '\\n' or '\\uxxxx' for a unicode character.
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int exampleString=0x7f010000;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -42,6 +75,7 @@ public final class R {
         public static final int activity_battle=0x7f030000;
         public static final int activity_menu=0x7f030001;
         public static final int activity_pass=0x7f030002;
+        public static final int sample_activity_view=0x7f030003;
     }
     public static final class menu {
         public static final int battle=0x7f070000;
@@ -88,4 +122,82 @@ public final class R {
          */
         public static final int AppTheme=0x7f060001;
     }
+    public static final class styleable {
+        /** Attributes that can be used with a ActivityView.
+           <p>Includes the following attributes:</p>
+           <table>
+           <colgroup align="left" />
+           <colgroup align="left" />
+           <tr><th>Attribute</th><th>Description</th></tr>
+           <tr><td><code>{@link #ActivityView_exampleColor com.googlehack.battlemind:exampleColor}</code></td><td></td></tr>
+           <tr><td><code>{@link #ActivityView_exampleDimension com.googlehack.battlemind:exampleDimension}</code></td><td></td></tr>
+           <tr><td><code>{@link #ActivityView_exampleDrawable com.googlehack.battlemind:exampleDrawable}</code></td><td></td></tr>
+           <tr><td><code>{@link #ActivityView_exampleString com.googlehack.battlemind:exampleString}</code></td><td></td></tr>
+           </table>
+           @see #ActivityView_exampleColor
+           @see #ActivityView_exampleDimension
+           @see #ActivityView_exampleDrawable
+           @see #ActivityView_exampleString
+         */
+        public static final int[] ActivityView = {
+            0x7f010000, 0x7f010001, 0x7f010002, 0x7f010003
+        };
+        /**
+          <p>This symbol is the offset where the {@link com.googlehack.battlemind.R.attr#exampleColor}
+          attribute's value can be found in the {@link #ActivityView} array.
+
+
+          <p>Must be a color value, in the form of "<code>#<i>rgb</i></code>", "<code>#<i>argb</i></code>",
+"<code>#<i>rrggbb</i></code>", or "<code>#<i>aarrggbb</i></code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name com.googlehack.battlemind:exampleColor
+        */
+        public static final int ActivityView_exampleColor = 2;
+        /**
+          <p>This symbol is the offset where the {@link com.googlehack.battlemind.R.attr#exampleDimension}
+          attribute's value can be found in the {@link #ActivityView} array.
+
+
+          <p>Must be a dimension value, which is a floating point number appended with a unit such as "<code>14.5sp</code>".
+Available units are: px (pixels), dp (density-independent pixels), sp (scaled pixels based on preferred font size),
+in (inches), mm (millimeters).
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name com.googlehack.battlemind:exampleDimension
+        */
+        public static final int ActivityView_exampleDimension = 1;
+        /**
+          <p>This symbol is the offset where the {@link com.googlehack.battlemind.R.attr#exampleDrawable}
+          attribute's value can be found in the {@link #ActivityView} array.
+
+
+          <p>May be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+<p>May be a color value, in the form of "<code>#<i>rgb</i></code>", "<code>#<i>argb</i></code>",
+"<code>#<i>rrggbb</i></code>", or "<code>#<i>aarrggbb</i></code>".
+          @attr name com.googlehack.battlemind:exampleDrawable
+        */
+        public static final int ActivityView_exampleDrawable = 3;
+        /**
+          <p>This symbol is the offset where the {@link com.googlehack.battlemind.R.attr#exampleString}
+          attribute's value can be found in the {@link #ActivityView} array.
+
+
+          <p>Must be a string value, using '\\;' to escape characters such as '\\n' or '\\uxxxx' for a unicode character.
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name com.googlehack.battlemind:exampleString
+        */
+        public static final int ActivityView_exampleString = 0;
+    };
 }
