6fd26ce41b52b9844fe971aba04a979ad60dd9e7
brechang <brechang@seas.upenn.edu>
2014-02-07 23:18:42 -0500

    everything finally works

--- Battlemind/src/com/googlehack/battlemind/MenuActivity.java
+++ Battlemind/src/com/googlehack/battlemind/MenuActivity.java
@@ -4,41 +4,44 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 import android.widget.Button;
 
 public class MenuActivity extends Activity {
-	private Button mixed;
-	private Button numerical;
-	private Button alpha;
+//	private Button mixed;
+//	private Button numerical;
+//	private Button alpha;
 	private static final String GAMEMODE = "GameMode";
 	//game modes mixed = 0, numerical = 1, alpha = 2
 	private int buttontype;
-	private Intent i = new Intent(this, BattleActivity.class);
-
+	private Intent i;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_menu);
+		i = new Intent(this, BattleActivity.class);
+
 		
-		mixed.findViewById(R.id.mixed_button);
-		numerical.findViewById(R.id.numeric_button);
-		alpha.findViewById(R.id.alpha_button);
+		setContentView(R.layout.activity_menu);
+////		
+//		mixed.findViewById(R.id.mixed_button);
+//		numerical.findViewById(R.id.numeric_button);
+//		alpha.findViewById(R.id.alpha_button);
 		
 	}
 	
-	public void alphaClicked() {
+	public void alphaClicked(View view) {
 		buttontype = 0;
 		i.putExtra(GAMEMODE, buttontype);
 		startActivityForResult(i, 0);
 	}
 	
-	public void numericalClicked() {
+	public void numericalClicked(View view) {
 		buttontype = 1;
 		i.putExtra(GAMEMODE, buttontype);
 		startActivityForResult(i, 0);
 	}
 	
-	public void mixedClicked() {
+	public void mixedClicked(View view) {
 		buttontype = 2;
 		i.putExtra(GAMEMODE, buttontype);
 		startActivityForResult(i, 0);
